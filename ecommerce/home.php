09361d3da96c233e4978ecef2334dc8054dd9969
kalyani-1209 <bommareddykalyani1@gmail.com>
2025-11-03 16:30:16 +0530

    Delete ecommerce directory

--- ecommerce/home.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($products as $product): ?>
-    <div class="card">
-        <h3><?= $product['name']; ?></h3>
-        <p>Price: $<?= $product['price']; ?></p>
-        <p><?= $product['description']; ?></p>
-    </div>
-<?php endforeach; ?>
